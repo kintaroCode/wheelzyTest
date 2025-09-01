5ec48459019110e81d54a2324e4fb92aa51359de
Isrrael <60905152+kintaroCode@users.noreply.github.com>
2025-09-01 00:01:20 -0300

    Update Question3.cs

--- Question3.cs
+++ Question3.cs
@@ -1,6 +1,6 @@
-Wheelzy Test
+//Wheelzy Test
 
-Pregunta 3 .- mejorar --->  
+//Pregunta 3 .- mejorar --->  
 public void UpdateCustomersBalanceByInvoices(List<Invoice> invoices)
 {
   foreach (var invoice in invoices)
@@ -11,18 +11,18 @@ public void UpdateCustomersBalanceByInvoices(List<Invoice> invoices)
   }
 }
 
-¿qué hace el método ?
- a.- actualiza el balance en cuenta de los clientes. 
+//¿qué hace el método ?
+// a.- actualiza el balance en cuenta de los clientes. 
 
-¿qué datos necesita?
- a.- una lista de facturas con id de clientes. 
- b.- buscar los clientes a BD.
- c.- guardar el nuevo valor.
+//¿qué datos necesita?
+// a.- una lista de facturas con id de clientes. 
+// b.- buscar los clientes a BD.
+// c.- guardar el nuevo valor.
 
-¿problema principal?
-esta todo dentro de un foreach
+//¿problema principal?
+//esta todo dentro de un foreach
 
-a -> sacaria la llamda de "var customer = dbContext.Customers.SingleOrDefault(invoice.CustomerId.Value)"  y "dbContext.SaveChanges" fuera del foreach para no llamar dos veces en cada     ocasiona a base de datos ;
-b.-> haria solo un savechages();
-c.-> No es necesario llamar a todos los clientes de la base, solo lo involucrados en las facturas. 
+//a -> sacaria la llamda de "var customer = dbContext.Customers.SingleOrDefault(invoice.CustomerId.Value)"  y "dbContext.SaveChanges" fuera del foreach para no llamar dos veces en cada     ocasiona a base de datos ;
+//b.-> haria solo un savechages();
+//c.-> No es necesario llamar a todos los clientes de la base, solo lo involucrados en las facturas. 
 
